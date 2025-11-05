59c92855373456f2c9849f42fa43f93045165b21
Darwin <floresjack74@gmail.com>
2025-11-05 17:37:44 -0500

    Componentes Sidebar y Navbar credos con exito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,18 @@ import { CategoryComponent } from './components/inventario/category.component';
 import { Register } from '@features/auth/pages/register/register';
 import { LoginComponent } from '@features/auth/pages/login/login';
 import { ImportReportComponent } from './components/import-report/import-report.component';
+import { Dashboard } from './components/dashboard/dashboard';
+import { Sidebar } from '@shared/components/sidebar/sidebar';
+import { Navbar } from '@shared/components/navbar/navbar';
 
 export const routes: Routes = [
   { path:'',redirectTo:'register',pathMatch:'full'},
+  {path:'dashboard',component:Dashboard},
   { path: 'productos', component: ProductListComponent },
   { path: 'register', component: Register },
   { path: 'login', component:LoginComponent},
   { path: 'categorias', component:CategoryComponent},
-  { path: 'report', component: ImportReportComponent }
+  { path: 'report', component: ImportReportComponent },
+  {path:'sidebar',component:Sidebar},
+  {path:'navbar',component:Navbar}
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from '@shared/components/sidebar/sidebar';
 import { CategoryComponent } from './components/inventario/category.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet], // <-- quitar ImportReportComponent
+  imports: [RouterOutlet,Sidebar], // <-- quitar ImportReportComponent
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
