be8ddeef52deee477f9f61a7c1a21e020ca4168e
ElProta <jaimepercca2@gmail.com>
2025-12-02 12:45:04 -0500

    Remove eliminarProducto method from product service
    Removed the eliminarProducto method for product deletion.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -22,23 +22,20 @@ export class ProductosService {
   private http = inject(HttpClient);
   private apiUrl = 'https://stockify-backend-0r7c.onrender.com/api/productos/';
 
-  /** GET: obtener lista de productos */
+  
   getProductos(): Observable<Producto[]> {
     return this.http.get<Producto[]>(this.apiUrl);
   }
 
-  /** POST: crear un producto */
+  
   crearProducto(data: Producto): Observable<Producto> {
     return this.http.post<Producto>(this.apiUrl, data);
   }
 
-  /** PUT: actualizar un producto */
+ 
   actualizarProducto(id: number, data: Producto): Observable<Producto> {
     return this.http.put<Producto>(`${this.apiUrl}${id}/`, data);
   }
 
-  /** DELETE: eliminar un producto */
-  eliminarProducto(id: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}${id}/`);
-  }
+  
 }
